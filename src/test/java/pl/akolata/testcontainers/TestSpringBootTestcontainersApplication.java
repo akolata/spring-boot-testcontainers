0a7ae8f60c1abcca420c5639b2bceace4de39c9a
akolata <olek.tbg.konta@gmail.com>
2024-11-11 23:23:48 +0100

    Initial commit

---
+++ src/test/java/pl/akolata/testcontainers/TestSpringBootTestcontainersApplication.java
@@ -0,0 +1,11 @@
+package pl.akolata.testcontainers;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestSpringBootTestcontainersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(SpringBootTestcontainersApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
